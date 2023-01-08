bc7389a44d4e0a49fa1c1e6b5884ef155e19b330
Soumya Ranjan Kalia <a2soumya.ranjan@gmail.com>
2023-01-08 16:15:07 +0530

    done

--- db/Task.js
+++ db/Task.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema({
   task: String,
   details: String,
+  deadline:String
 });
 
 module.exports = mongoose.model("taskss", taskSchema);
